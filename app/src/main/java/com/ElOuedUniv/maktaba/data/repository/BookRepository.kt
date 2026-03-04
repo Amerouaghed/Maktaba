8f13dafd51e9620a8020423c57c51dff669d6baf
amerouaghed <asdasd444w@gmail.com>
2026-03-04 09:02:53 +0100

    Update booksList with detailed book entries

--- app/src/main/java/com/ElOuedUniv/maktaba/data/repository/BookRepository.kt
+++ app/src/main/java/com/ElOuedUniv/maktaba/data/repository/BookRepository.kt
@@ -23,12 +23,19 @@ class BookRepository {
      * )
      */
     private val booksList = listOf(
-        Book(isbn = "", title = "Clean Code", nbPages = 0),
-        Book(isbn = "", title = "The Pragmatic Programmer", nbPages = 0),
-        Book(isbn = "", title = "Design Patterns", nbPages = 0),
-        Book(isbn = "", title = "Refactoring", nbPages = 0),
-        Book(isbn = "", title = "Head First Design Patterns", nbPages = 0)
+        Book("978-0-13-468599-1", "Clean Code", 464),
+        Book("978-0-596-52068-7", "Head First Design Patterns", 694),
+        Book("978-0-201-63361-0", "Design Patterns", 395),
+        Book("978-1-491-94728-4", "Kotlin in Action", 360),
+        Book("978-1-59327-599-0", "Automate the Boring Stuff with Python", 592),
+
+        Book("978-0-321-35668-0", "Effective Java", 416),
+        Book("978-1-491-92428-5", "Android Programming", 812),
+        Book("978-0-13-235088-4", "Clean Architecture", 432),
+        Book("978-1-118-96914-3", "Android Development for Beginners", 720),
+        Book("978-1491950357", "Designing Data-Intensive Applications", 616)
     )
+
 
     /**
      * TODO for Students (TP1 - Exercise 2):
